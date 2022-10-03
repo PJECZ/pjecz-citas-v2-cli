239f4bca75321c7ae819add9133b77d64a743124
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2022-10-03 12:36:33 -0500

    Guardar CSV de oficinas

--- pjecz_citas_v2_cli/oficinas/app.py
+++ pjecz_citas_v2_cli/oficinas/app.py
@@ -1,6 +1,9 @@
 """
 CLI Oficinas App
 """
+import csv
+from datetime import datetime
+
 import rich
 import typer
 
@@ -17,12 +20,15 @@ def consultar(
     distrito_id: int = None,
     domicilio_id: int = None,
     estatus: str = None,
+    guardar: bool = False,
     limit: int = LIMIT,
     puede_agendar_citas: bool = True,
     offset: int = 0,
 ):
     """Consultar oficinas"""
     rich.print("Consultar oficinas...")
+
+    # Solicitar datos
     try:
         respuesta = get_oficinas(
             distrito_id=distrito_id,
@@ -35,8 +41,37 @@ def consultar(
     except CLIAnyError as error:
         typer.secho(str(error), fg=typer.colors.RED)
         raise typer.Exit()
+
+    # Encabezados
+    encabezados = ["ID", "Clave", "Distrito", "Descripcion", "P.A.C.", "Apertura", "Cierre", "L.P."]
+
+    # Guardar datos en un archivo CSV
+    if guardar:
+        fecha_hora = datetime.now().strftime("%Y%m%d%H%M%S")
+        nombre_archivo_csv = f"oficinas_{fecha_hora}.csv"
+        with open(nombre_archivo_csv, "w", encoding="utf-8") as archivo:
+            escritor = csv.writer(archivo)
+            escritor.writerow(encabezados)
+            for registro in respuesta["items"]:
+                escritor.writerow(
+                    [
+                        str(registro["id"]),
+                        registro["clave"],
+                        registro["distrito_nombre_corto"],
+                        registro["descripcion_corta"],
+                        "SI" if bool(registro["puede_agendar_citas"]) else "",
+                        registro["apertura"],
+                        registro["cierre"],
+                        str(registro["limite_personas"]),
+                    ]
+                )
+        rich.print(f"Datos guardados en el archivo {nombre_archivo_csv}")
+
+    # Mostrar la tabla
     console = rich.console.Console()
-    table = rich.table.Table("ID", "Clave", "Distrito", "Descripcion", "P.A.C.", "Apertura", "Cierre", "L.P.")
+    table = rich.table.Table()
+    for enca in encabezados:
+        table.add_column(enca)
     for registro in respuesta["items"]:
         table.add_row(
             str(registro["id"]),
@@ -49,4 +84,6 @@ def consultar(
             str(registro["limite_personas"]),
         )
     console.print(table)
+
+    # Mostrar el total
     rich.print(f"Total: [green]{respuesta['total']}[/green] oficinas")
